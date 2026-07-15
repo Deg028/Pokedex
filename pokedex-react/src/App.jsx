e2e95cfb9f896695419d23cfc6b05ca92690eeeb
Deg <davenrique15gomezaconcha@gmail.com>
2026-07-15 12:46:22 -0500

    cambio

--- pokedex-react/src/App.jsx
+++ pokedex-react/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import ThemeToggle from './components/ThemeToggle';
-import SearchBar from './components/SearchBar';
-import PokemonCard from './components/PokemonCard';
-import './index.css';
+import { useState } from "react";
+import ThemeToggle from "./components/ThemeToggle";
+import SearchBar from "./components/SearchBar";
+import PokemonCard from "./components/PokemonCard";
+import "./index.css";
 
 function App() {
   // 1. Reemplazamos las variables del DOM por Estados de React
@@ -15,16 +15,22 @@ function App() {
   const fetchPokemon = async (query, selectedGen) => {
     try {
       setError(false); // Limpiamos errores previos
-      
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
-      if (!response.ok) throw new Error("Pokémon no encontrado. Intenta de nuevo.");
-      
+
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${query}`,
+      );
+      if (!response.ok)
+        throw new Error("Pokémon no encontrado. Intenta de nuevo.");
+
       const data = await response.json();
-      
+
       const speciesResponse = await fetch(data.species.url);
       const species = await speciesResponse.json();
 
-      if (selectedGen && species.generation.name !== `generation-${selectedGen}`) {
+      if (
+        selectedGen &&
+        species.generation.name !== `generation-${selectedGen}`
+      ) {
         throw new Error("Pokémon no encontrado en esa generación");
       }
 
